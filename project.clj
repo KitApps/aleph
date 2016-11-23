f85f2f3b6cf0267d9937061232d01d5c2ee7213c
Oleh Palianytsia <oleh.palianytsia@gmail.com>
2016-11-23 17:44:20 +0200

    Update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aleph "0.4.2-alpha10"
+(defproject aleph "0.4.2-alpha10-complete-timeout"
   :description "a framework for asynchronous communication"
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"
                  "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
